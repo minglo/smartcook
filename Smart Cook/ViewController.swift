6f1f63c88b123f62413af7841ae42a8a925c9a4a
Ming Lo <minglo@syncbox.cn>
2015-05-15 01:43:11 +0800

    Initial commit

---
+++ Smart Cook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart Cook
+//
+//  Created by Ming Lo on 15/4/9.
+//  Copyright (c) 2015年 ashleywu-minglo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
